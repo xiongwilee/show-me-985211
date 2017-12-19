351ac2860d587b6cb5764bf16247376a1453a5da
xiongwilee <xiongwilee@foxmail.com>
2017-12-19 16:28:39 +0800

    upd: 添加自动打招呼的功能

--- static/js/inject.js
+++ static/js/inject.js
@@ -3,87 +3,115 @@
  * @author xiongwilee
  */
 var ShowMe985211 = (function() {
-    function showMe985211() {
-        this.list = [
-            '清华大学', '北京大学', '中国人民大学', '北京工业大学', '北京理工大学', '北京航空航天大学', '北京化工大学', '北京邮电大学', '对外经济贸易大学', '中国传媒大学', '中央民族大学', '中国矿业大学', '中央财经大学', '中国政法大学', '中国石油大学', '中央音乐学院', '北京体育大学', '北京外国语大学', '北京交通大学', '北京科技大学', '北京林业大学', '中国农业大学', '北京中医药大学', '华北电力大学', '北京师范大学', '中国地质大学', '复旦大学', '华东师范大学', '上海外国语大学', '上海大学', '同济大学', '华东理工大学', '东华大学', '上海财经大学', '上海交通大学', '南开大学', '天津大学', '天津医科大学', '河北工业大学', '重庆大学', '西南大学', '华北电力大学', '太原理工大学', '内蒙古大学', '大连理工大学', '东北大学', '辽宁大学', '大连海事大学', '吉林大学', '东北师范大学', '延边大学', '东北农业大学', '东北林业大学', '哈尔滨工业大学', '哈尔滨工程大学', '南京大学', '东南大学', '苏州大学', '河海大学', '中国药科大学', '中国矿业大学', '南京师范大学', '南京理工大学', '南京航空航天大学', '江南大学', '南京农业大学', '浙江大学', '安徽大学', '合肥工业大学', '中国科学技术大学', '厦门大学', '福州大学', '南昌大学', '山东大学', '中国海洋大学', '中国石油大学', '郑州大学', '武汉大学', '华中科技大学', '中国地质大学', '华中师范大学', '华中农业大学', '中南财经政法大学', '武汉理工大学', '湖南大学', '中南大学', '湖南师范大学', '中山大学', '暨南大学', '华南理工大学', '华南师范大学', '广西大学', '四川大学', '西南交通大学', '电子科技大学', '西南财经大学', '四川农业大学', '云南大学', '贵州大学', '西北大学', '西安交通大学', '西北工业大学', '陕西师范大学', '西北农林科大', '西安电子科技大学', '长安大学', '兰州大学', '新疆大学', '石河子大学', '海南大学', '宁夏大学', '青海大学', '西藏大学', '第二军医大学', '第四军医大学', '国防科学技术大学'
-        ];
+  function showMe985211() {
+    this.list = [
+      '清华大学', '北京大学', '中国人民大学', '北京工业大学', '北京理工大学', '北京航空航天大学', '北京化工大学', '北京邮电大学', '对外经济贸易大学', '中国传媒大学', '中央民族大学', '中国矿业大学', '中央财经大学', '中国政法大学', '中国石油大学', '中央音乐学院', '北京体育大学', '北京外国语大学', '北京交通大学', '北京科技大学', '北京林业大学', '中国农业大学', '北京中医药大学', '华北电力大学', '北京师范大学', '中国地质大学', '复旦大学', '华东师范大学', '上海外国语大学', '上海大学', '同济大学', '华东理工大学', '东华大学', '上海财经大学', '上海交通大学', '南开大学', '天津大学', '天津医科大学', '河北工业大学', '重庆大学', '西南大学', '华北电力大学', '太原理工大学', '内蒙古大学', '大连理工大学', '东北大学', '辽宁大学', '大连海事大学', '吉林大学', '东北师范大学', '延边大学', '东北农业大学', '东北林业大学', '哈尔滨工业大学', '哈尔滨工程大学', '南京大学', '东南大学', '苏州大学', '河海大学', '中国药科大学', '中国矿业大学', '南京师范大学', '南京理工大学', '南京航空航天大学', '江南大学', '南京农业大学', '浙江大学', '安徽大学', '合肥工业大学', '中国科学技术大学', '厦门大学', '福州大学', '南昌大学', '山东大学', '中国海洋大学', '中国石油大学', '郑州大学', '武汉大学', '华中科技大学', '中国地质大学', '华中师范大学', '华中农业大学', '中南财经政法大学', '武汉理工大学', '湖南大学', '中南大学', '湖南师范大学', '中山大学', '暨南大学', '华南理工大学', '华南师范大学', '广西大学', '四川大学', '西南交通大学', '电子科技大学', '西南财经大学', '四川农业大学', '云南大学', '贵州大学', '西北大学', '西安交通大学', '西北工业大学', '陕西师范大学', '西北农林科大', '西安电子科技大学', '长安大学', '兰州大学', '新疆大学', '石河子大学', '海南大学', '宁夏大学', '青海大学', '西藏大学', '第二军医大学', '第四军医大学', '国防科学技术大学'
+    ];
 
-        this.config = {
-            'www.zhipin.com': {
-            	// 类名
-                className: 'show_me_985211',
-                // 是否延迟，不配置则不延迟
-                timer: 2000,
-                // 获取列表的选择器
-                domListSelector: 'a[data-suid]'
-            }
-        };
+    this.config = {
+      'www.zhipin.com': {
+        // 类名
+        className: 'show_me_985211',
+        // 是否延迟，不配置则不延迟
+        timer: 2000,
+        // 获取列表的选择器
+        domListSelector: 'a[data-suid]',
+        // 匹配到list之后的回调
+        onAfterMatchAll: function(domList) {
+          var domLen = domList.length;
+
+          if (domLen < 1) return;
+
+          if (window.confirm('找到了' + domLen + '个符合条件的候选人，是否自动打招呼？')) {
+            domList.forEach(function(item) {
+              var sayHiDom = item.parentElement.querySelector('.btn-greet');
+              if (sayHiDom && sayHiDom.click) {
+                sayHiDom.click();
+              }
+            });
+          }
+        },
+        // 匹配到其中一个item之后的回调
+        onAfterMatchItem: function(item) {}
+      }
+    };
+
+    this.itemCfg = this.config[window.location.host];
+  }
+
+  showMe985211.prototype.init = function() {
+    // 载入CSS
+    this.initCSS();
+    // 设置头部：标题等
+    this.render();
+  };
+  showMe985211.prototype.render = function() {
+    var me = this;
+
+    if (me.itemCfg.timer) {
+      setInterval(function() {
+        me.generator.call(me, me.itemCfg);
+      }, me.itemCfg.timer);
+    } else {
+      me.generator(me.itemCfg);
     }
 
-    showMe985211.prototype.init = function() {
-        // 载入CSS
-        this.initCSS();
-        // 设置头部：标题等
-        this.render();
-    };
-    showMe985211.prototype.render = function() {
-        var me = this;
+  };
+  showMe985211.prototype.generator = function(config) {
+    var me = this;
+    var domList = me.selectDom(config.className, config.domListSelector);
+    if (!domList || domList.length === 0) return;
 
-        var itemCfg = me.config[window.location.host];
-        if (itemCfg.timer) {
-            setInterval(function() {
-                me.generator.call(me, itemCfg);
-            }, itemCfg.timer);
-        } else {
-            me.generator(itemCfg);
-        }
+    // 添加获取完成所有dom时候的钩子
+    if (me.itemCfg.onAfterMatchAll) {
+      me.itemCfg.onAfterMatchAll.call(me, domList);
+    }
 
-    };
-    showMe985211.prototype.generator = function(config) {
-        var me = this;
-        var domList = me.selectDom(config.className, config.domListSelector);
-        if (!domList || domList.length === 0) return;
+    domList.forEach(function(item) {
+      item.classList.add(config.className);
 
-        domList.forEach(function(item) {
-            item.classList.add(config.className);
-        });
-    };
-    showMe985211.prototype.selectDom = function(className, domListSelector) {
-        var me = this;
+      // 添加获取完成所有dom时候的钩子
+      if (me.itemCfg.onAfterMatchItem) {
+        me.itemCfg.onAfterMatchItem.call(me, item);
+      }
+    });
+  };
+  showMe985211.prototype.selectDom = function(className, domListSelector) {
+    var me = this;
 
-        var domList = document.querySelectorAll(domListSelector);
-        var matchList = [];
-        if (domList.length === 0) return matchList;
+    var domList = document.querySelectorAll(domListSelector);
+    var matchList = [];
+    if (domList.length === 0) return matchList;
 
-        domList.forEach(function(item) {
-            if (item.classList.contains(className)) return;
+    domList.forEach(function(item) {
+      if (item.classList.contains(className)) return;
 
-            var text = item.innerText;
+      var text = item.innerText;
 
-            if (!text) return;
+      if (!text) return;
 
-            // 如果包含this.list中的文字则说明匹配到了985211院校
-            if (me.list.some(function(subItem) { return text.indexOf(subItem) > -1; })) {
-                matchList.push(item);
-            }
-        });
+      // 如果包含this.list中的文字则说明匹配到了985211院校
+      if (me.list.some(function(subItem) { return text.indexOf(subItem) > -1; })) {
+        matchList.push(item);
+      }
+    });
 
-        return matchList;
+    return matchList;
 
-    };
-    /**
-     * 载入CSS
-     */
-    showMe985211.prototype.initCSS = function() {
-        this.linkDOM = this.linkDOM || document.createElement('link');
-        this.linkDOM.rel = 'stylesheet';
-        this.linkDOM.href = chrome.extension.getURL('static/css/inject.css');
+  };
+  /**
+   * 载入CSS
+   */
+  showMe985211.prototype.initCSS = function() {
+    this.linkDOM = this.linkDOM || document.createElement('link');
+    this.linkDOM.rel = 'stylesheet';
+    this.linkDOM.href = chrome.extension.getURL('static/css/inject.css');
 
-        // 把link文件放在</body>和</html>之间，以避免回车搜索时干掉DOM
-        document.body.parentElement.appendChild(this.linkDOM);
-    };
+    // 把link文件放在</body>和</html>之间，以避免回车搜索时干掉DOM
+    document.body.parentElement.appendChild(this.linkDOM);
+  };
 
-    return (new showMe985211());
+  return (new showMe985211());
 })();
 
 ShowMe985211.init();
