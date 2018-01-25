443102bad89eadb028352efd8484071aa2363a8d
xiongwilee <xiongwilee@foxmail.com>
2018-01-25 10:21:32 +0800

    upd: 添加拉勾的自动打招呼的功能

--- static/js/inject.js
+++ static/js/inject.js
@@ -12,6 +12,8 @@ var ShowMe985211 = (function() {
       'www.zhipin.com': {
         // 类名
         className: 'show_me_985211',
+        // 是否严格匹配
+        isStrict: false,
         // 是否延迟，不配置则不延迟
         timer: 2000,
         // 获取列表的选择器
@@ -22,7 +24,9 @@ var ShowMe985211 = (function() {
 
           if (domLen < 1) return;
 
-          if (window.confirm('找到了' + domLen + '个符合条件的候选人，是否自动打招呼？')) {
+          var confirmText = '找到了' + domLen + '个符合条件的候选人，是否自动打招呼？' + '\n' +
+            '(不自动打招呼系统也可以帮您自动高亮符合要求的候选人)';
+          if (window.confirm(confirmText)) {
             domList.forEach(function(item) {
               var sayHiDom = item.parentElement.querySelector('.btn-greet');
               if (sayHiDom && sayHiDom.click) {
@@ -40,6 +44,86 @@ var ShowMe985211 = (function() {
 
           return true;
         }
+      },
+      'easy.lagou.com': {
+        // 类名
+        className: 'show_me_985211',
+        // 是否严格匹配
+        isStrict: true,
+        // 是否延迟，不配置则不延迟
+        timer: false,
+        // 获取列表的选择器
+        domListSelector: '.result_list_item',
+        // 匹配到list之后的回调
+        onAfterMatchAll: function(domList) {
+          var domLen = domList.length;
+          // 通过URL中的positionname或者keyword字段来匹配职位名称
+          var keyWord = this.search.positionname || this.search.keyword || '';
+
+          function auto(index) {
+            var domItem = domList[index];
+            if (!domItem) return;
+
+            // 获取“和TA联系”的按钮，并点击
+            var addChatBtn = domItem.querySelector('.add-chat-list');
+            if (addChatBtn && addChatBtn.click) { addChatBtn.click(); }
+
+            // 一秒钟之后查询页面中的职位并模拟点击发送
+            setTimeout(function() {
+              var positionMode = document.getElementById('chatWithTaPop');
+              if (!positionMode || positionMode.style.display == 'none') return;
+
+              var positionList = positionMode.querySelectorAll('.position-item');
+              if (!positionList || positionList.length == 0) return;
+
+              var positionItem;
+              for (var i = 0; i < positionList.length; i++) {
+                positionItem = positionList[i];
+                if (positionItem.innerText.indexOf(keyWord) > -1) {
+                  break;
+                }
+              }
+
+              // 选中职位
+              // 可以通过URL中的positionname参数来指定职位
+              if (!positionItem || !positionItem.click) return;
+              positionItem.click();
+
+              // 发送职位
+              var sendChatBtn = positionMode.querySelector('.can-click');
+              if (!sendChatBtn || !sendChatBtn.click) return;
+              sendChatBtn.click();
+
+              // 500ms之后递归执行
+              setTimeout(function() {
+                auto(index + 1);
+              }, 500);
+            }, 1000);
+          }
+
+          if (domLen < 1) return;
+
+          var confirmText = '职位：' + keyWord + '\n' +
+            '找到了' + domLen + '个符合条件的候选人，是否自动“和TA聊聊”？' + '\n' +
+            '(点击“取消”也会帮您自动高亮符合要求的候选人)';
+          if (window.confirm(confirmText)) {
+            auto(0);
+          }
+        },
+        // 匹配到其中一个item之后的回调
+        onAfterMatchItem: function(item) {},
+        // 在自定义判断之前获取到了一个item之后的回调
+        onBeforeMatchItem: function(item) {
+          var text = item.innerText;
+          if (text.indexOf("继续聊聊") > -1) {
+            return false;
+          }
+
+          var spreadOutDetailBtn = item.querySelector('.spreadIn');
+          if (spreadOutDetailBtn && spreadOutDetailBtn.click) {
+            spreadOutDetailBtn.click();
+          }
+        }
       }
     };
 
@@ -47,8 +131,12 @@ var ShowMe985211 = (function() {
   }
 
   showMe985211.prototype.init = function() {
+    // 获取请求权参数
+    this.search = this.queryParse();
+
     // 载入CSS
     this.initCSS();
+
     // 设置头部：标题等
     this.render();
   };
@@ -72,7 +160,7 @@ var ShowMe985211 = (function() {
     var domList = preDomList.filter(function(item) {
       // 添加获取完成所有dom时候的钩子
       if (me.itemCfg.onAfterMatchItem) {
-        return me.itemCfg.onAfterMatchItem.call(me, item);
+        return me.itemCfg.onAfterMatchItem.call(me, item) !== false;
       } else {
         return true;
       }
@@ -99,12 +187,19 @@ var ShowMe985211 = (function() {
     domList.forEach(function(item) {
       if (item.classList.contains(className)) return;
 
+      if (me.itemCfg.onBeforeMatchItem) {
+        if (me.itemCfg.onBeforeMatchItem.call(me, item) === false) return;
+      }
+
       var text = item.innerText;
 
       if (!text) return;
 
       // 学历信息必须包含“本科、硕士、博士”
       if (!/本科|硕士|博士/g.test(text)) return;
+
+      // 学历信息必须包含“专科”则直接返回
+      if (me.itemCfg.isStrict && /大专/g.test(text)) return;
 
       // 如果包含this.list中的文字，则说明匹配到了985211院校
       if (me.list.some(function(subItem) { return text.indexOf(subItem) > -1; })) {
@@ -125,6 +220,22 @@ var ShowMe985211 = (function() {
 
     // 把link文件放在</body>和</html>之间，以避免回车搜索时干掉DOM
     document.body.parentElement.appendChild(this.linkDOM);
+  };
+
+
+  /**
+   * 解析获取当前页面的GET请求参数
+   * @return {Object} 请求参数对象
+   */
+  showMe985211.prototype.queryParse = function(str) {
+    // 页面url参数集合
+    var dataObj = {};
+    var url = str || window.location.href;
+    // 正则会匹配 ?a=b&c=d
+    url.replace(/([^?=&#]+)=([^?=&#]+)/g, function() {
+      dataObj[arguments[1]] = decodeURIComponent(arguments[2]);
+    });
+    return dataObj;
   };
 
   return (new showMe985211());
